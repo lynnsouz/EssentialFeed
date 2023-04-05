11c5548740d662f6d3d451907e7c8212abfc4b9e
Lynneker Souza <lynnsouz@gmail.com>
2023-04-05 19:08:31 -0300

    Replace stub configs

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -31,10 +31,12 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
 
         var capturedErrors = [RemoteFeedLoader.Error]()
         sut.load { capturedErrors.append($0) }
+
+        let clientError = NSError (domain: "Test", code: 0)
+        client.completions[0](clientError)
 
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -50,12 +52,10 @@ class RemoteFeedLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         private(set) var requestedURLs = [URL]()
-        var error: Error?
+        private(set) var completions = [(Error) -> Void]()
 
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
