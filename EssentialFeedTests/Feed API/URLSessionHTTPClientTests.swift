4402204ab0292c7c360d53c0dbed52fe8833a5b2
Lynneker Souza <lynnsouz@gmail.com>
2023-05-01 18:30:45 -0300

    Add test for Memory Leak

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -40,7 +40,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
 
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
 
         wait(for: [exp], timeout: 1.0)
     }
@@ -50,10 +50,8 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError (domain: "any error", code: 1)
         URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
 
-        let sut = URLSessionHTTPClient()
-
         let exp = expectation(description: "Wait for te block")
-        sut.get(from: url)  { result in
+        makeSUT().get(from: url)  { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -68,6 +66,21 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> URLSessionHTTPClient {
+        let sut = URLSessionHTTPClient()
+        trackForMemoryLeaks (sut, file: file, line: line)
+        return sut
+    }
+
+    private func trackForMemoryLeaks(_ instance: AnyObject,
+                                     file: StaticString = #file,
+                                     line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instarde should have been deallocated. Potential memory leak.",
+                         file: file, line: line)
+        }
+    }
 
     private class URLProtocolStub: URLProtocol {
         private static var requestObserver: ((URLRequest) -> Void)?
