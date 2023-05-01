4566fc129194e7c7e82f606ae90f729915f921d9
Lynneker Souza <lynnsouz@gmail.com>
2023-05-01 16:11:27 -0300

    Add URLSession tests

---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+
+class URLSessionHTTClient {
+    private let session: URLSession
+
+    init(session: URLSession) {
+        self.session = session
+    }
+
+    func get(from url: URL) {
+        session.dataTask(with: url) {_, _, _ in }
+    }
+}
+
+final class URLSessionHTTPClientTests: XCTestCase {
+    func test() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTClient(session: session)
+
+        sut.get(from: url)
+
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+
+    // MARK: - Helpers
+
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+
+
+        override func dataTask(with url: URL) -> URLSessionDataTask {
+            receivedURLs.append(url)
+
+            return FakeURLSessionDataTask()
+        }
+    }
+
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+}
