add2a8af3c9231a66fd97dfcf8920f888054e651
Lynneker Souza <lynnsouz@gmail.com>
2023-05-01 14:20:33 -0300

    FeedItemsMapper file.

--- EssentialFeed/Feed Feature/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed Feature/Feed API/RemoteFeedLoader.swift
@@ -33,32 +33,3 @@ public final class RemoteFeedLoader {
         }
     }
 }
-
-private class FeedItemsMapper {
-    private struct FeedItemsHTTPResponse: Decodable {
-        let items: [Item]
-    }
-
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-
-        var item: FeedItem {
-            FeedItem(id: id,
-                     description: description,
-                     location: location,
-                     imageURL: image)
-        }
-    }
-    static var OK_200: Int { return 200 }
-
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200,
-              let response = try? JSONDecoder().decode(FeedItemsHTTPResponse.self, from: data)
-        else { throw RemoteFeedLoader.Error.invalidData }
-        return response.items.map({ $0.item })
-    }
-}
-
