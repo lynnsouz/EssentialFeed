99236f632ff44834cff07bd2a491bff5c619643d
Lynneker Souza <lynnsouz@gmail.com>
2023-05-01 18:25:22 -0300

    Add Setup and Teardown

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -20,9 +20,17 @@ class URLSessionHTTPClient {
 }
 
 final class URLSessionHTTPClientTests: XCTestCase {
-    func test_getFromURL_performsGETRequestWithURL() {
-        URLProtocolStub.startInterceptinaRequests()
 
+    override class func setUp() {
+        super.setUp()
+        URLProtocolStub.startInterceptinaRequests()
+    }
+
+    override class func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    func test_getFromURL_performsGETRequestWithURL() {
         let url = URL(string: "http://any-url.com")!
         let exp = expectation(description: "Wait for te block")
 
@@ -35,11 +43,9 @@ final class URLSessionHTTPClientTests: XCTestCase {
         URLSessionHTTPClient().get(from: url) { _ in }
 
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
 
     func test_getFromURL_failsOnRequestError () {
-        URLProtocolStub.startInterceptinaRequests()
         let url = URL(string: "http://any-url.com")!
         let error = NSError (domain: "any error", code: 1)
         URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
@@ -59,7 +65,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
 
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
 
     // MARK: - Helpers
