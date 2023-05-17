f1163ead1a1fcf3ca880dee664a41d5fd3ef7f13
Lynneker Souza <lynnsouz@gmail.com>
2023-05-17 15:19:48 -0300

    RemoteFeedItem rename

--- EssentialFeed/Feed Feature/Feed API/RemotefeedItem.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-internal struct RemoteFeedImage: Decodable {
-    internal let id: UUID
-    internal let description: String?
-    internal let location: String?
-    internal let image: URL
-}
