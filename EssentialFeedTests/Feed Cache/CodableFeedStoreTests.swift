9a67a376a50b4978747e708a087ad14b23d623af
Lynneker Souza <lynnsouz@gmail.com>
2023-06-25 16:14:24 -0300

    Retrieving after inserting to empty cache state.

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -2,12 +2,55 @@ import XCTest
 import EssentialFeed
 
 class CodableFeedStore {
+    private struct Cache: Codable {
+        let feed: [LocalFeedImage]
+        let timestamp: Date
+    }
+    private let storeURL = FileManager.default.urls(for: . documentDirectory,
+                                                    in: .userDomainMask)
+        .first!.appendingPathComponent("image-feed.store")
+
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        guard let data = try? Data(contentsOf: storeURL) else {
+            completion(.empty)
+            return
+        }
+
+        let decoder = JSONDecoder()
+        let cache = try! decoder.decode(Cache.self, from: data)
+        completion(.found(feed: cache.feed, timestamp: cache.timestamp))
+    }
+
+    func insert(_ feed: [LocalFeedImage],
+                timestamp: Date,
+                completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
 
 final class CodableFeedStoreTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        let storeURL = FileManager.default.urls(for: .documentDirectory,
+                                                in: .userDomainMask)
+            .first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+
+    override class func tearDown() {
+        super.tearDown()
+
+        let storeURL = FileManager.default.urls(for: .documentDirectory,
+                                                in: .userDomainMask)
+            .first!.appendingPathComponent("image-feed.store")
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = CodableFeedStore()
 
@@ -22,7 +65,7 @@ final class CodableFeedStoreTests: XCTestCase {
             exp.fulfill()
         }
 
-        wait(for: [exp])
+        wait(for: [exp], timeout: 1.0)
     }
 
     func test_retrieve_hasNoSideEffectOnEmptyCache() {
@@ -41,7 +84,32 @@ final class CodableFeedStoreTests: XCTestCase {
             }
         }
 
-        wait(for: [exp])
+        wait(for: [exp], timeout: 1.0)
+    }
+
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+        let sut = CodableFeedStore()
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+
+        let exp = expectation(description: "Wait for cache retrieval")
+        sut.insert(feed, timestamp: timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+
+            sut.retrieve { result in
+                switch result {
+                case let .found(retrievedFeed, retrievedTimestamp):
+                    XCTAssertEqual(retrievedFeed, feed)
+                    XCTAssertEqual(retrievedTimestamp, timestamp)
+                    break
+                default:
+                    XCTFail("Expected found result with \(feed) and timestamp \(timestamp), got \(result) instead.")
+                }
+                exp.fulfill()
+            }
+        }
+
+        wait(for: [exp], timeout: 1.0)
     }
 
     // MARK: - Helpers
