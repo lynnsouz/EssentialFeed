002303a9542a55a2af4ee17860dab9dc9fa7769c
Lynneker Souza <lynnsouz@gmail.com>
2023-06-25 19:45:11 -0300

    Add information on `get` method.

--- EssentialFeed/Feed Feature/Feed API/HTTPClient.swift
+++ EssentialFeed/Feed Feature/Feed API/HTTPClient.swift
@@ -6,5 +6,7 @@ public enum HTTPCLientResult {
 }
 
 public protocol HTTPClient {
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func get(from url: URL, completion: @escaping (HTTPCLientResult) -> Void)
 }
