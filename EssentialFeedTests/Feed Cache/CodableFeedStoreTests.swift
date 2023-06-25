7dc40ae4c1f69933a72527291939c1471cc68d7d
Lynneker Souza <lynnsouz@gmail.com>
2023-06-25 16:38:48 -0300

    Centralize url to saca cache tests.

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -62,19 +62,13 @@ final class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        let storeURL = FileManager.default.urls(for: .documentDirectory,
-                                                in: .userDomainMask)
-            .first!.appendingPathComponent("image-feed.store")
-        try? FileManager.default.removeItem(at: storeURL)
+        try? FileManager.default.removeItem(at: storeURL())
     }
 
-    override class func tearDown() {
+    override func tearDown() {
         super.tearDown()
 
-        let storeURL = FileManager.default.urls(for: .documentDirectory,
-                                                in: .userDomainMask)
-            .first!.appendingPathComponent("image-feed.store")
-        try? FileManager.default.removeItem(at: storeURL)
+        try? FileManager.default.removeItem(at: storeURL())
     }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -142,11 +136,14 @@ final class CodableFeedStoreTests: XCTestCase {
 
     private func makeCodableFeedStoreSUT(file: StaticString = #file,
                                          line: UInt = #line) -> CodableFeedStore {
-        let storeURL = FileManager.default.urls(for: . documentDirectory,
-                                                in: .userDomainMask)
-            .first!.appendingPathComponent("image-feed.store")
-        let sut = CodableFeedStore(storeURL: storeURL)
+        let sut = CodableFeedStore(storeURL: storeURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+
+    private func storeURL() -> URL {
+        FileManager.default.urls(for: . documentDirectory,
+                                 in: .userDomainMask)
+        .first!.appendingPathComponent("image-feed.store")
     }
 }
