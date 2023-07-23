a4a43b7293733a9614ac03d1e2ea6f4a448690dd
Lynneker Souza <lynnsouz@gmail.com>
2023-07-22 21:03:31 -0300

    Retry failed image load on retry action

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -214,6 +214,33 @@ final class FeedViewControllerTests: XCTestCase {
                        "Expected retry action once image loading completes with invalid image data")
     }
 
+    func test_feedImageViewRetryAction_retriesImageLoad() {
+        let image0 = makeImage(url: URL(string: "http://url-0.com")!)
+        let image1 = makeImage(url: URL(string: "http://url-1.com")!)
+        let (sut, loader) = makeSUT()
+
+        sut.loadViewIfNeeded()
+        loader.completeFeedLoading(with: [image0, image1])
+
+        let view0 = sut.simulateFeedImageViewVisible(at: 0)
+        let view1 = sut.simulateFeedImageViewVisible(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url],
+                       "Expected two image URL request for the two visible views")
+
+        loader.completeImageLoadingWithError(at: 0)
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url],
+                       "Expected only two image URL requests before retry action")
+
+        view0?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url, image0.url],
+                       "Expected third imageURL request after first view retry action")
+
+        view1?.simulateRetryAction()
+        XCTAssertEqual(loader.loadedImageURLs, [image0.url, image1.url, image0.url, image1.url],
+                       "Expected fourth imageURL request after second view retry action")
+    }
+
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file,
                          line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -364,6 +391,9 @@ private extension FeedViewController {
 }
 
 private extension FeedImageCell {
+    func simulateRetryAction() {
+        feedImageRetryButton.simulateTap()
+    }
     var isShowingLocation: Bool {
         return !locationContainer.isHidden
     }
@@ -389,6 +419,16 @@ extension UIRefreshControl {
         allTargets.forEach { target in
             actions(forTarget: target,
                     forControlEvent: .valueChanged)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
+
+private extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
                 (target as NSObject).perform(Selector($0))
             }
         }
