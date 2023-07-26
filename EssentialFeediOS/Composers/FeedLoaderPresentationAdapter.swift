9fed3270d5f5901411a24abdaa6d9908a798312f
Lynneker Souza <lynnsouz@gmail.com>
2023-07-25 22:59:20 -0300

    New files. Organize project.

---
+++ EssentialFeediOS/Composers/FeedLoaderPresentationAdapter.swift
@@ -0,0 +1,28 @@
+import Foundation
+import EssentialFeed
+
+final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
+    private let feedLoader: FeedLoader
+    var presenter: FeedPresenter?
+
+    init(feedloader: FeedLoader) {
+        self.feedLoader = feedloader
+    }
+
+    func didRequestFeedRefresh() {
+        presenter?.didStartLoadingFeed()
+        feedLoader.load { [weak self] result in
+            self?.handleLoadedResult(result)
+        }
+    }
+
+    private func handleLoadedResult(_ result: Result<[FeedImage], Error>) {
+        switch result {
+        case let .success(feed):
+            presenter?.didFinishLoadingFeed (with: feed)
+        case let .failure(error):
+            presenter?.didFinishLoadingFeed(with: error)
+        }
+    }
+}
+
