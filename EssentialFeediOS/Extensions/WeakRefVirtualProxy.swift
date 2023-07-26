838e05fd70fe09d917670914d3ec0226b049a043
Lynneker Souza <lynnsouz@gmail.com>
2023-07-25 22:53:55 -0300

    new file for `WeakRefVirtualProxy`

---
+++ EssentialFeediOS/Extensions/WeakRefVirtualProxy.swift
@@ -0,0 +1,21 @@
+import Foundation
+import EssentialFeed
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(FeedLoadingViewModel(isLoading: viewModel.isLoading))
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
